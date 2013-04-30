8ca6697a7c525f2a049e10697729d427b65b4240
Ricardo Cerqueira <cyanogenmod@cerqueira.org>
2013-04-30 02:25:48 +0100

    Fix NPE in SecureCameraActivity init
    Change-Id: I2632d7baff0e08c0533157b149972466cedb679b

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -1954,7 +1954,9 @@ public class PhotoModule
         waitCameraStartUpThread();
 
         // Disable no-hands mode, and kill any pending voice listeners
-        mPhotoControl.resetNoHandsShutter(true);
+        if (mPhotoControl != null) {
+            mPhotoControl.resetNoHandsShutter(true);
+        }
 
         // When camera is started from secure lock screen for the first time
         // after screen on, the activity gets onCreate->onResume->onPause->onResume.
