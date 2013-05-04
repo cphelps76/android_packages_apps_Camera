73d52c9f950e43431b8733cf404daa0c0ebadfba
Steve Kondik <shade@chemlab.org>
2013-05-04 03:17:08 -0700

    camera: Add support for ZSL on new Samsung devices
     * The camera driver on new Samsung devices requires that we send a
       magic command to the hardware in order to get ZSL behavior with
       continuous preview. On previous devices, this worked as long as the
       "cam_mode" parameter was zero (instead of null) and "camera-mode" is
       set to 1. On JF, we need to do all of these things.
     * The command we need to send is 1508, but I do not know what this
       value actually corresponds to. It was discovered by reverse
       engineering.
    
    Change-Id: I6bccc9cd228784045eef81719892121749af4fa5

--- src/com/android/camera/PhotoModule.java
+++ src/com/android/camera/PhotoModule.java
@@ -2557,6 +2557,9 @@ public class PhotoModule
         }
 
         if (Util.enableZSL()) {
+            if (Util.sendMagicSamsungZSLCommand()) {
+                mCameraDevice.sendMagicSamsungZSLCommand();
+            }
             // Switch on ZSL mode
             mParameters.set("camera-mode", "1");
         } else {
